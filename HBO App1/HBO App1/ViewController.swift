21bb59fe50b1819418987c3dfd2d02bf523003ea
Kavindu Nimsara <kevindunimasara@gmail.com>
2020-01-06 03:30:38 -0800

    1

---
+++ HBO App1/HBO App1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HBO App1
+//
+//  Created by Kavindu Nimsara on 1/6/20.
+//  Copyright © 2020 Kavindu Nimsara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HBO App1/HBO App1Tests/HBO_App1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  HBO_App1Tests.swift
+//  HBO App1Tests
+//
+//  Created by Kavindu Nimsara on 1/6/20.
+//  Copyright © 2020 Kavindu Nimsara. All rights reserved.
+//
+
+import XCTest
+@testable import HBO_App1
+
+class HBO_App1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
